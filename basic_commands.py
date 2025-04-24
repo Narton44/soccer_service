0dac5cb3927e53758663ac5426031f45d32c5d37
Narton44 <sosokom5522@gmail.com>
2025-04-24 17:06:21 +0300

    Created basic_commands

---
+++ basic_commands.py
@@ -0,0 +1,9 @@
+# python -m venv venv
+# venv\Scripts\activate
+# pip install Django
+# django-admin startproject ...
+# py manage.py startapp ...
+# py mange.py makemigrations
+# py mange.py migrate
+# py manage.py createsuperuser
+# py manage.py runserver
