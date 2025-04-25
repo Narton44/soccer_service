f1871fc48a0e5699526770f5b9836b4e311c6da6
Narton44 <sosokom5522@gmail.com>
2025-04-25 10:05:18 +0300

    допилил basic_commands

--- basic_commands.py
+++ basic_commands.py
@@ -1,8 +1,9 @@
-# python -m venv venv
-# venv\Scripts\activate
-# pip install Django
-# django-admin startproject ...
-# py manage.py startapp ...
+# 1 django-admin startproject ...  и создаётся папка(корневой кталог проекта)
+# потом переходим в созданную папку (каталог)
+# 2 python -m venv venv создаём в нём вируальную среду
+# 3 venv\Scripts\activate активируем её
+# 4 pip install Django устанавливаем Джанго
+# 5 py manage.py startapp ... создаём приложение проекта
 # py manage.py makemigrations
 # py manage.py migrate
 # py manage.py createsuperuser
