0d2ed3ef49376d516452a521a5b588927e37268b
gef3dx <gef3dx@gmail.com>
2025-05-26 19:12:01 +0300

    fix tamletes and add views userlistview for admin

--- users/urls.py
+++ users/urls.py
@@ -6,12 +6,14 @@ from django.contrib.auth.views import (
     PasswordResetCompleteView
 
     )
-from users.views import RegisterUser
+from users.views import RegisterUser, UserListView
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name="register"),
+    path('list/', UserListView.as_view(), name="userlist"),
+
     #path("password_reset/", PasswordResetView.as_view(), name="password_reset"),
     # path(
     #     "password_reset/done/",
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from django.shortcuts import redirect
 
 from users.models import CustomUser
 from users.forms import CustomUserForm
@@ -8,4 +9,27 @@ class RegisterUser(CreateView):
     form_class = CustomUserForm
     context_object_name = "create_user"
     success_url = "/"
-    template_name = "register.html"
+    template_name = "user/register.html"
+
+class UserListView(ListView):
+    model = CustomUser
+    context_object_name = "users"
+    template_name = "user/userslist.html"
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_staff:
+            self.object = None
+            return super().get(request, *args, **kwargs)
+        else:
+            return redirect('home')
+            
+    def post(self, request, *args, **kwargs):
+        if self.request.user.is_staff:
+            self.object = None
+            return super().post(request, *args, **kwargs)
+        else:
+            return redirect('home')
+    
+    def get_queryset(self, **kwargs):
+        queryset = CustomUser.objects.all()
+        return queryset
