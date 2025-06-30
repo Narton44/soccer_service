70ceef4912909bb0b5877ae0107c7f4d176100d9
gef3dx <gef3dx@gmail.com>
2025-06-30 20:36:33 +0300

    edit bokings views

--- field_rental/views/bookings.py
+++ field_rental/views/bookings.py
@@ -6,6 +6,9 @@ from django.views.generic import (
     UpdateView
 )
 from django.urls import reverse_lazy
+from datetime import datetime, timedelta
+from django.utils import timezone
+
 
 
 from ..models import Booking, Fields
@@ -28,9 +31,28 @@ class BookingsListView(ListView):
     context_object_name = "bookings"
     template_name = "bookings/bookinglist.html"
 
+    def get_queryset(self):
+        today_start = timezone.now().replace(
+            hour=0, minute=0, second=0, microsecond=0
+        )
+        today_end = today_start + timedelta(days=7)
+        
+        queryset =  Booking.objects.filter(field__pk = self.kwargs["pk"])
+
+        return queryset.filter(created_at__range=(today_start, today_end))
+    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['field'] = Fields.objects.get(pk = self.kwargs["pk"])
+
+        date_lable = []
+        i = 0
+        for date in range(7):
+            date_lable.append(str(datetime.today() + timedelta(i))[:11])
+            i += 1
+        
+        context['label_data'] = date_lable
         return context
 
 class BookingsDetailView(DetailView):
