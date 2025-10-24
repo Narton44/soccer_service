979be3aeb7481565f805da9e0c32a4868dee7b82
Narton44 <sosokom5522@gmail.com>
2025-10-24 17:49:11 +0300

    add in settings postgress

--- core/settings.py
+++ core/settings.py
@@ -93,12 +93,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'field',
+            'USER': 'field',
+            'PASSWORD': 'St3679vdS',
+            'HOST': 'localhost',  # Or the IP address/hostname of your PostgreSQL server
+            'PORT': '5432',       # Default PostgreSQL port
+        }
     }
-}
 
 
 # Password validation
